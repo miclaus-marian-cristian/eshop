dab673da652b374302f91de4f0491b7bf0e65b96
DevForReal <marianmcristian@gmail.com>
2024-07-02 21:14:23 +0300

    Created the repository

---
+++ category-catalog/src/main/java/com/mcm/category_catalog/CategoryRepository.java
@@ -0,0 +1,15 @@
+package com.mcm.category_catalog;
+
+import java.util.UUID;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mcm.category_catalog.entity.Category;
+
+@Repository
+public interface CategoryRepository extends ReactiveMongoRepository<Category, UUID>{
+	
+	
+
+}
