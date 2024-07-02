e5005cef6436fcff6b8d020c7272735ddd18e67d
DevForReal <marianmcristian@gmail.com>
2024-07-02 20:37:54 +0300

    created category-catalog service

---
+++ category-catalog/src/main/java/com/mcm/category_catalog/CategoryCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.mcm.category_catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoryCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategoryCatalogApplication.class, args);
+	}
+
+}
---
+++ category-catalog/src/test/java/com/mcm/category_catalog/CategoryCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcm.category_catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoryCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
