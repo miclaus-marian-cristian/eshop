cf93250d47b74a0c2d6f2b311c8d9514a587d18c
DevForReal <marianmcristian@gmail.com>
2024-08-02 00:12:23 +0300

    Update ProductRepository.java

--- product-catalog/src/main/java/com/mcm/product_catalog/repository/ProductRepository.java
+++ product-catalog/src/main/java/com/mcm/product_catalog/repository/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.mcm.product_catalog.repository;
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.mcm.product_catalog.entity.Product;
 
+@Repository
 public interface ProductRepository extends ReactiveMongoRepository<Product, String>{
 
 }
