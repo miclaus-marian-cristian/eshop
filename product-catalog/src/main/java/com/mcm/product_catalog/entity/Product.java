0857990ee5803cbf7710c6e14b91d8ae3947eb04
DevForReal <marianmcristian@gmail.com>
2024-08-02 00:04:33 +0300

    Update Product.java

--- product-catalog/src/main/java/com/mcm/product_catalog/entity/Product.java
+++ product-catalog/src/main/java/com/mcm/product_catalog/entity/Product.java
@@ -6,13 +6,26 @@ import java.util.Map;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Document(collection = "products")
 public class Product {
 	
 	@Id
+	@EqualsAndHashCode.Include
     private String id;
     private String name;
     private double price;
